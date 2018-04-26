5dbef26fbf27a4f14c649e4f2544cbd8ae596925
ChenqXu <cx2198@columbia.edu>
2018-04-25 21:37:24 -0400

    log

---
+++ 3DUI_Project/Assets/Scripts/ARScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		VuforiaBehaviour.Instance.enabled = true/false;
+		VuforiaRuntime.Instance.InitVuforia ();
+		GetComponent<VuforiaBehaviour> ().enabled = true;
+		GetComponent<DefaultInitializationErrorHandler> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		//Go back to main menu: need to deinit vuforia camera
+//		VuforiaRuntime.Instance.Deinit ();
+//		GetComponent<VuforiaBehaviour> ().enabled = false;
+//		GetComponent<DefaultInitializationErrorHandler> ().enabled = false;
+		
+	}
+}
---
+++ 3DUI_Project/Assets/Scripts/WandSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WandSelection : MonoBehaviour {
+
+	public int state = 0;
+
+	GameObject canv;
+
+	// Use this for initialization
+	void Start () {
+		canv = GameObject.Find("Canvas");
+		canv.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// im trying to modify the text here, according to the collider.
+
+		switch(state) {
+		case 1:
+			canv.SetActive (true);
+			GameObject.Find ("PanelTopic").GetComponent<Text> ().text = "gold";
+			break;
+		case 2:
+			canv.SetActive (true);
+			GameObject.Find ("PanelTopic").GetComponent<Text> ().text = "silver";
+			break;
+		case 3:
+			canv.SetActive (true);
+			GameObject.Find ("PanelTopic").GetComponent<Text> ().text = "silver";
+			break;
+		default:
+			canv.SetActive (false);
+			break;
+		}
+	}
+}
